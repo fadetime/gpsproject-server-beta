0bede3542663825f6bb38c8466e608e736645790
fadetime <fadetime@163.com>
2018-08-06 12:01:04 +0800

    alpha V0.4.2
    限制合作商查找数据的日期

--- controllers/client-company.js
+++ controllers/client-company.js
@@ -2,38 +2,47 @@ const Product = require('../models/client-driver')
 
 exports.client_company_get = (req, res, next) => {
     let startdate = new Date(req.body.startdate).getTime()
-    let enddate = startdate + 86400000
-    startdate = new Date(startdate).toISOString()
-    enddate = new Date(enddate).toISOString()
-    Product.find({ "missiondate": { "$gte": startdate, "$lt": enddate } })
-        .then(doc => {
-            if (!doc) {
-                res.send({
-                    code: 1,
-                    msg: '未找到该任务信息'
-                })
-            } else {
-                let temparr = []
-                doc.forEach(x => {
-                    x.missionclient.forEach(y => {
-                        if (y.clientbserve == req.body.username) {
-                            temparr.push(y)
-                        }
+    let today = new Date().getTime()
+    let configDay = 15 * 24 * 3600 * 1000
+    if (startdate < today - configDay) {
+        res.send({
+            code: 1,
+            msg: '历史数据已删除'
+        })
+    } else {
+        let enddate = startdate + 86400000
+        startdate = new Date(startdate).toISOString()
+        enddate = new Date(enddate).toISOString()
+        Product.find({ "missiondate": { "$gte": startdate, "$lt": enddate } })
+            .then(doc => {
+                if (!doc) {
+                    res.send({
+                        code: 1,
+                        msg: '未找到该任务信息'
                     })
-                })
-                res.send({
-                    code:0,
-                    msg:'查找成功',
-                    doc:temparr
-                })
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.send({
-                code: 2,
-                msg: '删除时出现错误',
-                error: err
+                } else {
+                    let temparr = []
+                    doc.forEach(x => {
+                        x.missionclient.forEach(y => {
+                            if (y.clientbserve == req.body.username) {
+                                temparr.push(y)
+                            }
+                        })
+                    })
+                    res.send({
+                        code: 0,
+                        msg: '查找成功',
+                        doc: temparr
+                    })
+                }
             })
-        })
+            .catch(err => {
+                console.log(err)
+                res.send({
+                    code: 2,
+                    msg: '删除时出现错误',
+                    error: err
+                })
+            })
+    }
 }
