0ed14533864b825f9b098b556fc5f39bf38b5192
fadetime <fadetime@163.com>
2019-06-14 16:20:55 +0800

    alphaV0.35.2
    修复 白班客户端报表API

--- controllers/dayShiftMission.js
+++ controllers/dayShiftMission.js
@@ -206,77 +206,16 @@ exports.thisMonthInfoReport = (req, res, next) => {
     endDate = new Date(endDate).toDateString()
     endDate = new Date(endDate).setMonth(req.body.myMonth)
     myDayShiftMission
-        .countDocuments({orderDate:{"$gte": startDate, "$lt": endDate}})
-        .then(allNum => {
-            if(allNum){//所有单数
-                myDayShiftMission
-                    .countDocuments({isIncreaseOrder:'true',orderDate:{"$gte": startDate, "$lt": endDate}})
-                    .then(increaseNum => {
-                        if(increaseNum){//加单数
-                            myDayShiftMission
-                                .countDocuments({isIncreaseOrder:'false',orderDate:{"$gte": startDate, "$lt": endDate}})
-                                .then(recoupNum => {
-                                    if(recoupNum){//补单数
-                                        myDayShiftMission
-                                            .countDocuments({isIncreaseOrder:'return',orderDate:{"$gte": startDate, "$lt": endDate}})
-                                            .then(returnNum => {
-                                                if(returnNum){//退单数
-                                                    myDayShiftMission
-                                                        .countDocuments({isIncreaseOrder:'other',orderDate:{"$gte": startDate, "$lt": endDate}})
-                                                        .then(otherNum => {//其他单数
-                                                            if(otherNum){
-                                                                res.send({
-                                                                    code:0,
-                                                                    allNum:allNum,
-                                                                    increaseNum:increaseNum,
-                                                                    recoupNum:recoupNum,
-                                                                    returnNum:returnNum,
-                                                                    otherNum:otherNum
-                                                                })
-                                                            }else{
-                                                                res.send({
-                                                                    code:1,
-                                                                    msg:'catch an error while find other order documents'
-                                                                })
-                                                            }
-                                                        })
-                                                        .catch(err => {
-                                                            console.log(err)
-                                                        })
-                                                }else{
-                                                    res.send({
-                                                        code:1,
-                                                        msg:'catch an error while find return order documents'
-                                                    })
-                                                }
-                                            })
-                                            .catch(err => {
-                                                console.log(err)
-                                            })
-                                    }else{
-                                        res.send({
-                                            code:1,
-                                            msg:'catch an error while find recoup order documents'
-                                        })
-                                    }
-                                })
-                                .catch(err => {
-                                    console.log(err)
-                                })
-                        }else{
-                            res.send({
-                                code:1,
-                                msg:'catch an error while find increase order documents'
-                            })
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
+        .find({orderDate:{"$gte": startDate, "$lt": endDate}})
+        .then(thisMonthInfo => {
+            if(thisMonthInfo.length === 0){
+                res.send({
+                    code: 1
+                })
             }else{
                 res.send({
-                    code:1,
-                    msg:'catch an error while find this month all documents'
+                    code: 0,
+                    doc: thisMonthInfo
                 })
             }
         })
